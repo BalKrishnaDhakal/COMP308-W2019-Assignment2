e2435bae442be5f79ca6689c9fe9750209d103e6
Bal krishna Dhakal <bdhakal1@my.centennialcollege.ca>
2019-03-28 00:01:26 -0400

    added login and register functionalites

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -26,6 +26,14 @@ import { ContactDeleteComponent } from './contacts/contact-delete/contact-delete
 
 // Services
 import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from './services/auth.service';
+import { JwtModule, JwtHelperService, JwtInterceptor } from '@auth0/angular-jwt';
+
+
+export function jwtTokenGetter() {
+  return localStorage.getItem('id_token');
+}
+
 
 
 @NgModule({
@@ -51,7 +59,12 @@ import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messag
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    FlashMessagesModule
+    FlashMessagesModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: jwtTokenGetter
+      }
+    })
   ],
   providers: [FlashMessagesService],
   bootstrap: [AppComponent]
