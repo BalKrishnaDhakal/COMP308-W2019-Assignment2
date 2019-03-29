0395c008c0e92cb12b673f7d5c5b4e49a7ea6e1d
Bal krishna Dhakal <bdhakal1@my.centennialcollege.ca>
2019-03-29 00:23:11 -0400

    built for heroku deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
-    "URI": "mongodb://localhost/test",
+    //"URI": "mongodb://localhost/test",
     // "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson4b"
+    "URI": "mongodb://bal:b12345@ds034208.mlab.com:34208/comp308-assignment2",
     "secret": "SomeSecret"
 }
