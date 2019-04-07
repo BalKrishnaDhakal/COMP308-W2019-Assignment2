694c73a4c0a8da07dbf6847554244fa994fd3470
Bal krishna Dhakal <bdhakal1@my.centennialcollege.ca>
2019-04-06 23:11:00 -0400

    rebuilt and deployed

--- client/src/app/services/contact-list.service.ts
+++ client/src/app/services/contact-list.service.ts
@@ -14,7 +14,7 @@ export class  ContactListService {
   private user: User;
    private authToken:any= null;
 
-  //private endpoint = 'http://localhost:3000/api/contact-list/';
+ // private endpoint = 'http://localhost:3000/api/contact-list/';
  private endpoint='https://comp308-w2019-assignment2-bal.herokuapp.com/api/contact-list/';
 
 
